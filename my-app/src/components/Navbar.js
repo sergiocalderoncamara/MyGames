5b899ad5753fd02ac208d58cd6332c22a4a74489
Sergio Calderón <sergiocalde00@gmail.com>
2021-12-16 20:49:04 +0100

    Home

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -4,11 +4,11 @@ import { Routes, Route, Link, NavLink } from "react-router-dom";
 export default function Navbar(props) {
     return (
         <>
+            <h1>Mis Juegos</h1>
             <nav className="nav nav-tabs" role="tablist">
-                <h1>Mis Juegos</h1>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/tictactoe">TicTacToe</NavLink>
-                <NavLink to="/quiz">Quiz</NavLink>
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/tictactoe">TicTacToe</NavLink></li>
+                <li><NavLink to="/quiz">Quiz</NavLink></li>
             </nav>
         </>
     );
--- my-app/src/components/routes/Home.js
+++ my-app/src/components/routes/Home.js
@@ -4,6 +4,12 @@ export default function Home() {
     return (
         <main>
             <h2>Home</h2>
+            <h3>Bienvenido a MiniJuegos React</h3>
+            <p>Aquí podrás disfritar de una serie de MiniJuegos</p>
+            <p>Seleccione el juego al que quiera jugar en la barra de navegación</p>
+            <div>
+                <img src={'minijuegos.jpg'} className="img" alt="logo" />
+            </div>
         </main>
     );
 }
